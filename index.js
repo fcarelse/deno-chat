ef5b2058afd96dc33b0f0c9c728c8450b95d42f1
Francis Carelse <francis@carelse.org>
2021-09-11 16:15:29 +0100

    Quick update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-addEventListener("fetch", (event) => {
-	const response = new Response("Hello World 2.0!", {
-		headers: { "content-type": "text/plain" },
-	});
-	event.respondWith(response);
-})
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+app.use((ctx) => {
+  ctx.response.body = "Hello World the 3rd!";
+});
+
+addEventListener("fetch", app.fetchEventHandler());
